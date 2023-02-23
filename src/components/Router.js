82666024b6520aac46942785bb882a20db3bd47d
unknown <yjhan713@naver.com>
2023-02-23 19:34:38 +0900

    23.2.23

---
+++ src/components/Router.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+
+const AppRouter = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  return (
+    <Router>
+      <Routes>
+        {isLoggedIn ? (
+          <>
+            <Route path="/" element={<Home />} />
+          </>
+        ) : (
+          <Route path="/" element={<Auth />} />
+        )}
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
